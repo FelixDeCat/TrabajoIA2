dc13837763ee2443d56356ea80c1861eec559e57
Guillermo Felix Romero <guillermofelixromero@gmail.com>
2018-06-04 19:26:50 -0300

    Update FSM in Mobile dispositive
    Acomodando algunas cosas

--- Practico IA/Assets/FSM/FSM/EventFSM.cs
+++ Practico IA/Assets/FSM/FSM/EventFSM.cs
@@ -1,23 +1,14 @@
 using System;
-
-namespace IA2
-{
-	public class EventFSM<T>
-    {
+namespace IA2 {
+	public class EventFSM<T> {
 		private State<T> current;
-
-		public EventFSM(State<T> initial)
-        {
+		public EventFSM(State<T> initial) {
 			current = initial;
 			current.Enter(default(T));
 		}
-
-		public void SendInput(T input)
-        {
+		public void SendInput(T input) {
 			State<T> newState;
-
-			if (current.CheckInput(input, out newState))
-            {
+			if (current.CheckInput(input, out newState)) {
 				current.Exit(input);
 				current = newState;
 				current.Enter(input);
@@ -26,19 +17,11 @@ namespace IA2
 
 		public State<T> Current { get { return current; } }
 
-		public void Update()
-        {
+		public void Update() {
 			current.Update();
 		}
-
-        public void LateUpdate()
-        {
-            current.LateUpdate();
+        publvoid LateUpdate() { current.LateUpdate(); }
+        public void FixedUpdate() { current.FixedUpdate(); }
+	
         }
-
-        public void FixedUpdate()
-        {
-            current.FixedUpdate();
-        }
-	}
 }
