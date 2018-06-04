1e63a1ac8d6618c561236bfc6febc3ad90aad7e0
GastonSantamaria <38821268+GastonSantamaria@users.noreply.github.com>
2018-06-04 00:51:49 -0300

    IA 2

---
+++ Practico IA/Assets/Hero.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hero : MonoBehaviour, IUpdateble
+{
+    public float speed;
+    public float jumpForce;
+    public float cameraSpeedRotation;
+    Rigidbody _rb;
+
+    void Start()
+    {
+        // Agrego a la lista de los actualizables
+        UpdateManager.AddObjectUpdateable(this);
+
+
+        _rb = GetComponent<Rigidbody>();
+        _rb.freezeRotation = true;
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    // Nuevo update para los actualizables
+    public void OnUpdate()
+    {
+        transform.Rotate(new Vector3(0, Input.GetAxis("Mouse X"), 0) * cameraSpeedRotation, Space.World);
+    }
+
+    void Move()
+    {
+
+    }
+
+    
+
+    public void StopUpdating()
+    {
+        // Saco de las lista de actualizables y deja de actualizarse
+        UpdateManager.RemoveObjectUpdateable(this);
+    }
+         
+}
