31627320a3d35ae78ac0f1fc5acf1b38a7fd8fcb
felix-PC\felix <guillermofelixromero@gmail.com>
2018-06-07 18:26:56 -0300

    OfType , Select , ToList

--- Practico IA/Assets/Scripts/EnemyManager.cs
+++ Practico IA/Assets/Scripts/EnemyManager.cs
@@ -9,9 +9,16 @@ public class EnemyManager : MonoBehaviour {
 
     List<GridEntity> AllEntities;
 
+    List<Obstacle> obstaculos = new List<Obstacle>();
+    List<EnemySuicider> enemigos = new List<EnemySuicider>();
+
+    //IA2-P1 (OfType , Select , ToList)
     private void Start()
     {
         mySpatialGrid = GetComponent<SpatialGrid>();
         AllEntities = mySpatialGrid.GetEntities().ToList();
+
+        obstaculos = AllEntities.OfType<Obstacle>().Select(x => x.GetComponent<Obstacle>()).ToList();
+        enemigos = AllEntities.OfType<EnemySuicider>().Select(x => x.GetComponent<EnemySuicider>()).ToList();
     }
 }
