a772f34e020a4a29fc39211f9f7dbb79adc7754f
GastonSantamaria <38821268+GastonSantamaria@users.noreply.github.com>
2018-06-08 04:03:45 -0300

    Beam Update

---
+++ Practico IA/Assets/Scripts/Beam.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.Linq;
+using System;
+
+public class Beam : MonoBehaviour
+{
+    public float distanceToTarget;
+
+    // Use this for initialization
+    void Start ()
+    {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		
+	}
+
+    public void Shoot()
+    {
+        RaycastHit[] raycastInfo = Physics.RaycastAll(transform.position, transform.forward);
+        
+        // raycastInfo.Where(x => x.collider.gameObject.layer == Layers.ENEMY).Aggreg
+        
+    }
+}
